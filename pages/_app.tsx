022a6c7b5724ab07aa968ede1c3388e2292b6d2f
steve980102 <steve980102@gmail.com>
2024-03-18 08:24:45 +0900

    CSS applying to component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '@/shared/utils/prelude';
 import 'mac-scrollbar/dist/mac-scrollbar.css';
-import '../styles/globals.css';
+import '../styles/globals.css'
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
